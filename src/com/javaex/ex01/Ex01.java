a66b858a7a5fddfab8f43d445dc45ebdc228534d
'ib0729' <'ib0729@naver.com'>
2020-11-11 15:51:25 +0900

    1st commit!!

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,41 @@
+package com.javaex.ex01;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		int myAge ;
+		myAge = 25;
+		System.out.println(myAge);
+		
+		myAge = 26;
+		System.out.println(myAge);
+		
+		
+		//정수형
+		//자료형(int를 정확하게 써야한다)
+		int var01 = 3;
+		double var02 = -3.4;
+		
+		
+		//메모리 크기 확인
+		
+		//byte var04 = 12345678;
+		//short var05 = 12345678;
+		int var3 = 12345678;
+		long var06 = 12345678; 
+		System.out.println(var06);
+	
+		//long형
+		//int var07 = 2147483648; //int 의 범위보다 큰값
+		
+		
+		
+	    long var08 = 2147483648L; //long 형에 대입이 안됨 ???--> L 기호를 붙여야함
+	    
+	    
+	    long var09 = 2147483647L; //롱이 인티저 보다 크면 붙여주고 롱이 인티져보다 안크면 안붙여줘도됨 L
+	
+	    
+	}
+
+}
---
+++ src/com/javaex/ex01/Ex04.java
@@ -0,0 +1,31 @@
+package com.javaex.ex01;
+
+public class Ex04 {
+	
+	public static void main(String[] args) {
+		
+		
+		char ch01 = 'A';
+		System.out.println(ch01);
+		
+		char ch02 = '한';
+		System.out.println(ch02);
+		
+		
+		//비교
+		char ch03 = 65;  // 주의할것!!! 이렇게 넣는 경우는 거의 없음 있긴하지만..
+		System.out.println(ch03);
+		
+		//문자열
+		String str = "안녕하세요";
+		System.out.println(str);
+		
+		char str01='안';
+		String str02 = "안";
+		System.out.println(str01);
+		System.out.println(str02);
+		
+		//sysout + 컨트롤+스페이스바
+		
+	}
+}
